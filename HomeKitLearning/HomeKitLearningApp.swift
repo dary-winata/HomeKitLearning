e3fece55cf88c220bcae59c8ededfc953933bdf9
dary-winata <dary.winata15@gmail.com>
2023-08-26 16:15:21 +0700

    Initial Commit

---
+++ HomeKitLearning/HomeKitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeKitLearningApp.swift
+//  HomeKitLearning
+//
+//  Created by dary winata nugraha djati on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeKitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
